df941ecc8c6b4fbfe22cc32f4907dd14a4eab4d2
can gündoğdu <gundogdu.can@hotmail.com>
2025-05-13 23:49:10 +0300

    13.05.2025

---
+++ Services/Order/Core/ECommerce.Order.Application/Features/CQRS/Handlers/GetAddressByIdQueryHandler.cs
@@ -0,0 +1,24 @@
+﻿using ECommerce.Order.Application.Features.CQRS.Queries;
+using ECommerce.Order.Application.Features.CQRS.Results;
+using ECommerce.Order.Application.Interfaces;
+using ECommerce.Order.Domain.Entities;
+using Mapster;
+
+namespace ECommerce.Order.Application.Features.CQRS.Handlers
+{
+    public class GetAddressByIdQueryHandler(IRepository<Address> _repository)
+    {
+        public async Task<GetAddressByIdQueryResult> Handle(GetAddressByIdQuery query)
+        {
+            var value = await _repository.GetByIdAsync(query.Id);
+            
+            // Check if the address exists
+            if (value == null)
+            {
+                throw new Exception("Address not found");
+            }
+
+            return value.Adapt<GetAddressByIdQueryResult>();
+        }
+    }
+}
---
+++ Services/Order/Core/ECommerce.Order.Application/Features/CQRS/Handlers/GetAddressQueryHandler.cs
@@ -0,0 +1,20 @@
+﻿using ECommerce.Order.Application.Features.CQRS.Results;
+using ECommerce.Order.Application.Interfaces;
+using ECommerce.Order.Domain.Entities;
+using Mapster;
+using System.Reflection.Metadata.Ecma335;
+
+namespace ECommerce.Order.Application.Features.CQRS.Handlers
+{
+    public class GetAddressQueryHandler(IRepository<Address> _repository)
+    {
+
+        public async Task<List<GetAddressQueryResult>> Handle()
+        {
+            var values = await _repository.GetAllAsync();
+
+            var results = values.Adapt<List<GetAddressQueryResult>>();
+            return results;
+        }
+    }
+}
