4dd6c207348e75347d159e46ff7fe3a8736cc106
can gündoğdu <gundogdu.can@hotmail.com>
2025-05-28 23:24:24 +0300

    28.05.2025 63.Lesson end.

---
+++ Frondends/ECommerce.WebUI/Handlers/TokenHandler.cs
@@ -0,0 +1,39 @@
+﻿using ECommerce.WebUI.Services.IdentityServices;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.IdentityModel.Protocols.OpenIdConnect;
+using System.Net.Http.Headers;
+
+namespace ECommerce.WebUI.Handlers
+{
+    public class TokenHandler(IHttpContextAccessor _contextAccessor, IIdentityService _identityService) : DelegatingHandler
+    {
+
+        override protected async Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+        {
+            var accessToken = await _contextAccessor.HttpContext.GetTokenAsync(OpenIdConnectParameterNames.AccessToken);
+
+            request.Headers.Authorization = new AuthenticationHeaderValue("Bearer", accessToken);
+
+
+            var response = await base.SendAsync(request, cancellationToken);
+
+            if (response.StatusCode == System.Net.HttpStatusCode.Unauthorized)
+            {
+                var result = await _identityService.GetRefreshToken();
+                if (result)
+                {
+                    request.Headers.Authorization = new AuthenticationHeaderValue("Bearer", accessToken);
+                    await base.SendAsync(request, cancellationToken);
+                }
+            }
+
+            if (response.StatusCode == System.Net.HttpStatusCode.Unauthorized)
+            {
+
+                throw new Exception(response.StatusCode.ToString());
+            }
+
+            return response;
+        }
+    }
+}
--- Frondends/ECommerce.WebUI/Program.cs
+++ Frondends/ECommerce.WebUI/Program.cs
@@ -1,11 +1,16 @@
+using ECommerce.WebUI.Handlers;
 using ECommerce.WebUI.Services.CatalogServices.CategoryServices;
+using ECommerce.WebUI.Services.IdentityServices;
 using ECommerce.WebUI.Settings;
+using Microsoft.AspNetCore.Authentication.Cookies;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-
+builder.Services.AddHttpContextAccessor();
+builder.Services.AddScoped<TokenHandler>();
+builder.Services.AddScoped<IIdentityService, IdentityService>();
 builder.Services.Configure<ServiceApiSettings>(builder.Configuration.GetSection(nameof(ServiceApiSettings)));
 builder.Services.Configure<ClientSettings>(builder.Configuration.GetSection(nameof(ClientSettings)));
 
@@ -14,7 +19,17 @@ var serviceApiSettings = builder.Configuration.GetSection(nameof(ServiceApiSetti
 builder.Services.AddHttpClient<ICategoryService, CategoryService>(opt =>
 {
     opt.BaseAddress = new Uri(serviceApiSettings.Catalog.Path);
-});
+}).AddHttpMessageHandler<TokenHandler>();
+
+
+builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
+    .AddCookie(CookieAuthenticationDefaults.AuthenticationScheme, options =>
+    {
+        options.LoginPath = "/Login/Index";
+        options.ExpireTimeSpan = TimeSpan.FromMinutes(30); // Set the expiration time for the cookie
+        options.Cookie.Name = "ECommerceCookie"; // Set a custom cookie name
+        options.SlidingExpiration = true; // Enable sliding expiration
+    });
 
 
 builder.Services.AddControllersWithViews();
@@ -33,7 +48,7 @@ app.UseHttpsRedirection();
 app.UseStaticFiles();
 
 app.UseRouting();
-
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllerRoute(
